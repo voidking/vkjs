abd8c81fc5bbc9bad7dca8fa9bb1d21b3ce2cb70
voidking <voidking@qq.com>
2017-09-22 17:25:24 +0800

    预加载文件

--- src/vk.js
+++ src/vk.js
@@ -1,77 +1,35 @@
 (function () {
-    var moduleMap = {};
-    var fileMap = {};
-
-    var noop = function () {
+    var vk_config = {
+        root: '/',
+        path: {
+            'jquery': 'lib/jquery/jquery.min.js'
+        }
     };
-
     var vk = {
-        define: function(name, dependencies, factory) {
-            if (!moduleMap[name]) {
-                var module = {
-                    name: name,
-                    dependencies: dependencies,
-                    factory: factory
-                };
-
-                moduleMap[name] = module;
+        ids: [],
+        cachejs: function (script_filename){
+            cache_doc = document.getElementById(script_filename);
+            if(cache_doc){
+                return;
             }
-
-            return moduleMap[name];
+            var cache = document.createElement('object');
+            cache.data = vk_config.root + vk_config.path[script_filename];
+            cache.id = script_filename;
+            cache.width = 0;
+            cache.height = 0;    
+            document.getElementsByTagName('body')[0].appendChild(cache);
         },
-
-        use: function(name) {
-            var module = moduleMap[name];
-
-            if (!module.entity) {
-                var args = [];
-                for (var i=0; i<module.dependencies.length; i++) {
-                    if (moduleMap[module.dependencies[i]].entity) {
-                        args.push(moduleMap[module.dependencies[i]].entity);
-                    }
-                    else {
-                        args.push(this.use(module.dependencies[i]));
-                    }
-                }
-
-                module.entity = module.factory.apply(noop, args);
+        loadjs: function(script_filename) {
+            cache_doc = document.getElementById(script_filename);
+            if(!cache_doc){
+                this.cachejs(script_filename);
+                cache_doc = document.getElementById(script_filename);
             }
 
-            return module.entity;
-        },
-
-        require: function (pathArr, callback) {
-            for (var i = 0; i < pathArr.length; i++) {
-                var path = pathArr[i];
-
-                if (!fileMap[path]) {
-                    var head = document.getElementsByTagName('head')[0];
-                    var node = document.createElement('script');
-                    node.type = 'text/javascript';
-                    node.async = 'true';
-                    node.src = path + '.js';
-                    node.onload = function () {
-                        fileMap[path] = true;
-                        head.removeChild(node);
-                        checkAllFiles();
-                    };
-                    head.appendChild(node);
-                }
-            }
-
-            function checkAllFiles() {
-                var allLoaded = true;
-                for (var i = 0; i < pathArr.length; i++) {
-                    if (!fileMap[pathArr[i]]) {
-                        allLoaded = false;
-                        break;
-                    }
-                }
-
-                if (allLoaded) {
-                    callback();
-                }
-            }
+            var script = document.createElement('script');
+            script.setAttribute('type', 'text/javascript');
+            script.setAttribute('src', cache_doc.getAttribute('data'));
+            document.getElementsByTagName('body')[0].appendChild(script);
         }
     };
 
