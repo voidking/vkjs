70555fb72f87030782e8e58161045f302757f047
voidking <voidking@qq.com>
2017-09-22 21:00:16 +0800

    弃用cachejs，因为chrome中插入object后，插入script不会读取缓存

--- src/vk.js
+++ src/vk.js
@@ -7,34 +7,14 @@
         }
     };
     var vk = {
-        ids: [],
-        cachejs: function (script_filename){
-            cache_doc = document.getElementById(script_filename);
-            if(cache_doc){
+        loadjs: function(script_filename) {
+            script_doc = document.getElementById(script_filename);
+            if(script_doc){
                 return;
             }
-            var cache = document.createElement('object');
-            cache.data = vk_config.root + vk_config.path[script_filename];
-            cache.id = script_filename;
-            cache.width = 0;
-            cache.height = 0;    
-            document.getElementsByTagName('body')[0].appendChild(cache);
-            console.log('caching:'+script_filename);
-            cache.onload = cache.onreadystatechange = function(){
-                console.log('cached:'+script_filename);
-            };
-            
-        },
-        loadjs: function(script_filename) {
-            cache_doc = document.getElementById(script_filename);
-            if(!cache_doc){
-                this.cachejs(script_filename);
-                cache_doc = document.getElementById(script_filename);
-            }
-
             var script = document.createElement('script');
             script.setAttribute('type', 'text/javascript');
-            script.setAttribute('src', cache_doc.getAttribute('data'));
+            script.setAttribute('src', vk_config.root + vk_config.path[script_filename]);
             document.getElementsByTagName('body')[0].appendChild(script);
             console.log('loading:'+script_filename);
             script.onload = script.onreadystatechange = function(){
